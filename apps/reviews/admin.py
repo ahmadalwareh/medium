9b4fb0ef8619805df9ce9f5ea40e3c17641f0fbb
Ahmad Alwareh <55667622+ahmadalwareh@users.noreply.github.com>
2023-10-15 13:47:36 +0300

    Register Models

--- apps/reviews/admin.py
+++ apps/reviews/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product, Category, Company, ProductSize, ProductSite, Comment
+from django.contrib.auth.models import Group
 
-# Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Company)
+admin.site.register(ProductSize)
+admin.site.register(ProductSite)
+admin.site.register(Comment)
+
+admin.site.unregister(Group)
