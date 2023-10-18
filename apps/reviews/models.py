19bdd640bca11618d85685f0a3e527d6eda3927c
Ahmad Alwareh <55667622+ahmadalwareh@users.noreply.github.com>
2023-10-18 20:15:23 +0300

    Add Serializers.py

--- apps/reviews/models.py
+++ apps/reviews/models.py
@@ -1,3 +1,5 @@
+from time import timezone
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -62,3 +64,16 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.title
+
+
+# class Image(models.Model):
+#     name = models.CharField(max_length=255)
+#     image = VersatileImageField(
+#         'Image',
+#         upload_to='images/',
+#         ppoi_field='image_ppoi'
+#     )
+#     image_ppoi = PPOIField()
+#
+#     def __str__(self):
+#         return self.name
