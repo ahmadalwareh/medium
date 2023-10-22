3f8785227596962407944d5e85efea4d3dc14a64
Ahmad Alwareh <55667622+ahmadalwareh@users.noreply.github.com>
2023-10-22 19:34:08 +0300

    Add JWT Auth

--- medium/urls.py
+++ medium/urls.py
@@ -1,16 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
-from apps.reviews.views import ProductViewSet
+from apps.reviews.views import ProductViewSet, ImageViewSet, MyObtainTokenPairView
 from rest_framework.routers import DefaultRouter
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 router = DefaultRouter()
 router.register(r'product', ProductViewSet, basename='Product')
+router.register(r'image', ImageViewSet, basename='Image')
 
 urlpatterns = [
-    path(r'admin/', admin.site.urls),
-    path(r'', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('token/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
 ]
 
 if settings.DEBUG:
