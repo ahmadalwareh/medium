6f84900ba87f232d99559ff131103a7030e72d32
Ahmad Alwareh <55667622+ahmadalwareh@users.noreply.github.com>
2023-10-24 10:41:35 +0300

    Change Password func

--- apps/auth_app/views.py
+++ apps/auth_app/views.py
@@ -1,7 +1,9 @@
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated
 
-from .serializers import MyTokenObtainPairSerializer, RegisterSerializer
+from .serializers import (MyTokenObtainPairSerializer, RegisterSerializer,
+                          ChangePasswordSerializer, UpdateUserSerializer)
 
 from django.contrib.auth.models import User
 from rest_framework import generics
@@ -16,3 +18,17 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+
+class ChangePasswordView(generics.UpdateAPIView):
+
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ChangePasswordSerializer
+
+
+class UpdateProfileView(generics.UpdateAPIView):
+
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UpdateUserSerializer
